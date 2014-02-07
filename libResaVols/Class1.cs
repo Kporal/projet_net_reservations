2a9c93cfe18a8c81b350e9c549cc3ddc69f0ccfa
naoskev <vno4@laposte.net>
2014-02-07 11:06:09 +0100

    Ajout de la structure (BL, DAL et IHM)

---
+++ libResaVols/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetNet.Reservation.ReservationDAL.libResaVols
+{
+    public class Class1
+    {
+    }
+}
---
+++ libReservation/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetNet.Reservation.ReservationBL.libReservation
+{
+    public class Class1
+    {
+    }
+}
