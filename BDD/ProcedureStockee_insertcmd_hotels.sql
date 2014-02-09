5c22441d032c83507b4358d7c1e105af257d85d0
soncoerok <test@gmail.com>
2014-02-09 21:36:02 +0100

    Ajout des scripts pour la BDD

---
+++ BDD/ProcedureStockee_insertcmd_hotels.sql
@@ -0,0 +1,25 @@
+USE cmd_hotels;
+GO
+CREATE PROCEDURE dbo.insertCmdHotels
+		@FirstName varchar(50),
+		@LastName varchar(50), 
+		@Address varchar(50),
+		@PostalCode varchar(50),
+		@City varchar(50),
+		@Country varchar(50),
+		@Hotels_stars varchar(50),
+		@Hotels_city varchar(50),
+		@Hotels_country varchar(50),
+		@Hotels_price varchar(50),
+		@Hotels_dateStart varchar(50),
+		@Hotels_dateEnd varchar(50)
+AS
+
+BEGIN
+INSERT INTO cmd_hotels([client_firstname],[client_lastname],[client_address],[client_postal_code],
+[client_city],[client_country],[hotel_stars],[hotel_city],[hotel_country],[hotel_price],[hotel_dateStart],[hotel_dateEnd])
+VALUES (@FirstName,@LastName,@Address,@PostalCode,@City,@Country,@Hotels_stars,@Hotels_city,@Hotels_country,@Hotels_price,@Hotels_dateStart, @Hotels_dateEnd);
+END
+
+
+GO
---
+++ BDD/ProcedureStockee_insertcmd_vols.sql
@@ -0,0 +1,26 @@
+USE cmd_vols;
+GO
+CREATE PROCEDURE dbo.insertCmdVols
+
+		@FirstName varchar(50),
+		@LastName varchar(50), 
+		@Address varchar(50),
+		@PostalCode varchar(50),
+		@City varchar(50),
+		@Country varchar(50),
+		@Vols_name varchar(50),
+		@Vols_from varchar(50),
+		@Vols_to varchar(50),
+		@Vols_category varchar(50),
+		@Vols_DateStart varchar(50),
+		@Vols_DateEnd varchar(50)
+AS
+
+BEGIN
+INSERT INTO cmd_vols ([client_firstname],[client_lastname],[client_address],[client_postal_code],
+[client_city],[client_country],[vol_name],[vol_from],[vol_to],[vol_category],[vol_dateStart],[vol_dateEnd])
+VALUES (@FirstName,@LastName,@Address,@PostalCode,@City,@Country,@Vols_name,@Vols_from,@Vols_to,@Vols_category,@Vols_DateStart, @Vols_DateEnd);
+END
+
+
+GO
