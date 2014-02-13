d58f508f24c691e908b1ff1dd324824765c8b668
naoskev <vno4@laposte.net>
2014-02-13 19:35:09 +0100

    Modification de Hotel en rajoutant Country.

--- ModeleReservation/Hotel.cs
+++ ModeleReservation/Hotel.cs
@@ -6,12 +6,29 @@ using System.Threading.Tasks;
 
 namespace ProjetNet.Modele.ModeleReservation
 {
+    /// <summary>
+    /// Hotel de la reservation.
+    /// </summary>
     public class Hotel
     {
+        /// <summary>
+        /// Etoiles.
+        /// </summary>
         public string Stars { get; set;  }
 
+        /// <summary>
+        /// Ville.
+        /// </summary>
         public string City { get; set; }
 
+        /// <summary>
+        /// Pays.
+        /// </summary>
+        public string Country { get; set; }
+
+        /// <summary>
+        /// Prix.
+        /// </summary>
         public string Price { get; set; }
     }
 }
