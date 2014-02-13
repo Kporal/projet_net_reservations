38a867ce59083f961e2a9b2ee505217f3368093f
naoskev <vno4@laposte.net>
2014-02-13 19:33:31 +0100

    Signature de ModelReservation.

--- ModeleReservation/Properties/AssemblyInfo.cs
+++ ModeleReservation/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
 [assembly: AssemblyTitle("ModeleReservation")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Modeles pour la Reservation.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ModeleReservation")]
